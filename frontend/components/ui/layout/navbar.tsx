67cc0fb2789b03d64e3d46a4c25efc38d34d3d9f
bananahotcoco@gmail.com <salmanalprisi15@gmail.com>
2026-07-05 16:24:49 +0700

    first commit

---
+++ frontend/components/ui/layout/navbar.tsx
@@ -0,0 +1,8 @@
+
+import { useState } from "react";
+
+<header className="bg-white border-2 border-black">
+    <div className="max-w mx-auto px-8 py-2 flex items-center h-14 gap-4">
+        <div className=""></div>
+    </div>
+</header>
