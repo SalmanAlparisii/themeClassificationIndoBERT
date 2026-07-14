5f4df127276c30976cef77b83f80ca1332d37575
bananahotcoco@gmail.com <salmanalprisi15@gmail.com>
2026-07-14 21:03:28 +0700

    feat: add font poppins, jockeyOne, oswald

--- frontend/components/ui/layout/navbar.tsx
+++ frontend/components/ui/layout/navbar.tsx
@@ -1,8 +1,18 @@
+import {jockeyOneRegular} from "@/app/layout";
 
-import { useState } from "react";
-
-<header className="bg-white border-2 border-black">
-    <div className="max-w mx-auto px-8 py-2 flex items-center h-14 gap-4">
-        <div className=""></div>
-    </div>
-</header>
+function Navbar() {
+    return (
+    <header className="bg-white border-black border-b-2 rounded-xl">
+        <div className={`max-w mx-auto px-8 py-2 flex items-center h-14 gap-6 ${jockeyOneRegular.className}`}>
+            <div className="text-xl">Dashboard</div>
+            <div className="text-xl">Background</div>
+            <div className="text-xl">Research</div>
+            <div className="text-xl">Dataset</div>
+            <div className="text-xl">Methodology</div>
+            <div className="text-xl">Result</div>
+            <div className="text-xl">Try Model</div>
+        </div>
+    </header>
+    );
+}
+export default Navbar;
